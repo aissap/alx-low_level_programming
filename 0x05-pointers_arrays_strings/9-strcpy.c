1f7124fde05786218bb6a91cb0cc59c7a83bb951
aissa <jesus.jesus.core@gmail.com>
2023-06-26 15:09:49 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,19 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int | = 0;
+	int l = 0;
 	int x = 0;
 
-	while (*(src + |) != '\0')
+	while (*(src + l) != '\0')
 	{
-		|++;
+		l++;
 	}
+
 	for ( ; x < l ; x++)
 	{
 		dest[x] = src[x];
 	}
 	dest[l] = '\0';
+
 	return (dest);
 }
