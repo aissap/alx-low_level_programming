230b0a513d101119b0bd685652a80e2011b27c51
aissa <jesus.jesus.core@gmail.com>
2023-07-10 11:07:27 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,14 +2,30 @@
 #include <stdlib.h>
 
 /**
- * ch_free_grid - frees a 2 dementional array.
- * @grid: multide,entional array of char.
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
  * @height: height of the array.
  *
  * Return: no return
  */
-
 void ch_free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
+
+/**
+ * strtow - splits a string into words.
+ * @str: string.
+ *
+ * Return: pointer of an array of integers
+ */
+char **strtow(char *str)
 {
 	char **aout;
 	unsigned int c, height, i, j, a1;
@@ -17,7 +33,7 @@ void ch_free_grid(char **grid, unsigned int height)
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	for (c = height = 0; str[c] != '\0'; c++)
-		if (str[c] != '' && (str[c + 1] == '' || str[c + 1] == '\0'))
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			height++;
 	aout = malloc((height + 1) * sizeof(char *));
 	if (aout == NULL || height == 0)
