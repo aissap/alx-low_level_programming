93f1466758763bd5c201392334bf98f5f5eda046
aissap <jesus.jesus.core@gmail.com>
2023-10-16 17:49:16 -0400

    third

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+int my_div(int a, int b)
+{
+	return a / b;
+}
+
+int mod(int a, int b)
+{
+	return a % b;
+}
