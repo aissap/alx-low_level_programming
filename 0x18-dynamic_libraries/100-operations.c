d5a08e8ba7ca1346619c9b82cb7240ee7b0b391c
aissa zerrad <jesus.jesus.core@gmail.com>
2023-10-16 18:14:14 -0700

    done

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -3,25 +3,25 @@
 
 int add(int a, int b)
 {
-	return a + b;
+        return a + b;
 }
 
 int sub(int a, int b)
 {
-	return a - b;
+        return a - b;
 }
 
 int mul(int a, int b)
 {
-	return a * b;
+        return a * b;
 }
 
 int my_div(int a, int b)
 {
-	return a / b;
+        return a / b;
 }
 
 int mod(int a, int b)
 {
-	return a % b;
+        return a % b;
 }
