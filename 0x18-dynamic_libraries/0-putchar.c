54e577291f0877b10cca5cecd4d4c0fd81fd563a
aissa zerrad <jesus.jesus.core@gmail.com>
2023-10-17 16:59:03 -0700

    fixing errors

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -1,20 +1,17 @@
 #include "main.h"
-/**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
+#include <unistd.h>
 
-int main(void)
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success, 1 is returned.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	return (write(1, &c, 1));
+/** Use 'write' to print character to stdout
+ *
+ */
 }
