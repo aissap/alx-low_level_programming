2ed59a5cdf05208d3dc04485b7bf8cb91b734802
aissa zerrad <jesus.jesus.core@gmail.com>
2023-10-16 18:30:58 -0700

    commit

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,27 +1,21 @@
 #include "aissa.h"
-#include <stdio.h>
-
 int add(int a, int b)
 {
-        return a + b;
+	return a+b;
 }
-
 int sub(int a, int b)
 {
-        return a - b;
+	return a-b;
 }
-
 int mul(int a, int b)
 {
-        return a * b;
+	return a*b;
 }
-
-int my_div(int a, int b)
+int div(int a, int b)
 {
-        return a / b;
+	return a/b;
 }
-
 int mod(int a, int b)
 {
-        return a % b;
+	return a%b;
 }
