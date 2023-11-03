9a4cc9756890a95ccdd12877b084acac2bd85599
aissap <jesus.jesus.core@gmail.com>
2023-11-03 19:33:36 -0400

    hash tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table.
+ *
+ * Description: Key and value.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    int flag = 0;
+    hash_node_t *node;
+
+    if (ht == NULL)
+        return;
+
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node != NULL)
+        {
+            if (flag == 1)
+                printf(", ");
+            printf("'%s': '%s'", node->key, node->value);
+            flag = 1;
+            node = node->next;
+        }
+    }
+    printf("}\n");
+}
