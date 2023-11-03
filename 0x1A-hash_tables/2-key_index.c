51e7be41a24a6cd2daadeba7551e5e726c945ac7
aissap <jesus.jesus.core@gmail.com>
2023-11-03 19:00:00 -0400

    hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  * @key: The key to be hashed.
  * @size: The size of the hash table array.
  *
- * Return: The index at which the key/value pair should be stored.
+ * Return: The index at which the key/value pair should be store
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
