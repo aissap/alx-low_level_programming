1b72ca7c80317e143c0ce5f89073326801e2b2bf
aissap <jesus.jesus.core@gmail.com>
2023-11-03 19:04:23 -0400

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node, *temp;
 	char *value_copy;
-	unsigned long int index, i;
+	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
