063985c4b74db9ad32f9a02bdd6fc92c24574d27
aissap <jesus.jesus.core@gmail.com>
2023-11-03 18:55:38 -0400

    hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 -implementing the djb2 algorithm.
- * @str: The string.
- *
- * Return: The calculated hash.
- */
+* hash_djb2 -implementing the djb2 algorithm.
+* @str: The string.
+*
+* Return: The calculated hash.
+*/
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
